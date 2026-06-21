c1cfcb8d2c3fbb94f2aa21260b4e71ebaab7da10
Sheraz Dev <sherazk.dev@gmail.com>
2026-06-22 00:14:57 +0500

    v2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@ffmpeg-installer/ffmpeg",
+    "@ffprobe-installer/ffprobe",
+    "@ffmpeg-installer/win32-x64",
+    "@ffprobe-installer/win32-x64",
+  ],
 };
 
 export default nextConfig;
